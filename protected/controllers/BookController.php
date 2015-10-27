509b4904b6d41a7baf5354f3d2ac4e289ad279e2
sydorenkovd <sydorenkovd@gmail.com>
2015-10-27 17:26:26 +0200

    instance commit in phpshtorm

--- protected/controllers/BookController.php
+++ protected/controllers/BookController.php
@@ -1,9 +1,6 @@
 <?php
 class BookController extends Controller{
     public function actionIndex(){
-        $model = new Book();
-        $model->title = "War and Peace";
-        $model->author = "Tolstoy";
-        $model->save(false);
+        echo 111;
     }
 }
